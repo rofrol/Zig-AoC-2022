20bb544ecafb5d55230661ffd13c64b4f6654851
Roman Frołow <rofrol@gmail.com>
2022-12-13 19:28:03 +0100

    clean

--- src/day01.zig
+++ src/day01.zig
@@ -1,12 +1,7 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
 const util = @import("util.zig");
 const util2 = @import("util2.zig");
+
 const gpa = util.gpa;
 
 pub fn main() !void {}
@@ -22,11 +17,9 @@ fn solve(input: []const u8) ![2]u32 {
             std.sort.sort(u32, &maximum, {}, comptime std.sort.desc(u32));
             maximum[3] = 0;
         }
-        // print("\n{any}", .{maximum});
     }
 
     std.sort.sort(u32, &maximum, {}, comptime std.sort.desc(u32));
-    // print("\nresult: {any}", .{maximum});
     return .{ maximum[0], maximum[0] + maximum[1] + maximum[2] };
 }
 
@@ -39,35 +32,3 @@ test "test-input" {
     try std.testing.expectEqual(maximum2[0], 71780);
     try std.testing.expectEqual(maximum2[1], 212489);
 }
-
-// Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.sort;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
